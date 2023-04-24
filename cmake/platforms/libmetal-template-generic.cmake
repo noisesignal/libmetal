7ff149671f186743037ee476e32e1505905c707e
paul <paul@pc-hdy.home>
2023-04-24 15:34:55 +0200

    -mfpu=fpv4-sp-d16

--- cmake/platforms/libmetal-template-generic.cmake
+++ cmake/platforms/libmetal-template-generic.cmake
@@ -4,7 +4,7 @@
 set (CMAKE_SYSTEM_PROCESSOR "arm"            CACHE STRING "")
 set (MACHINE                "template"       CACHE STRING "")
 set (CROSS_PREFIX           "arm-none-eabi-" CACHE STRING "")
-set (CMAKE_C_FLAGS          "-mcpu=cortex-m4 -std=gnu11 -g3 -DNO_ATOMIC_64_SUPPORT"               CACHE STRING "")
+set (CMAKE_C_FLAGS          "-mcpu=cortex-m4 -std=gnu11 -g3 -DNO_ATOMIC_64_SUPPORT -mfpu=fpv4-sp-d16"               CACHE STRING "")
 
 include (cross-generic-gcc)
 
