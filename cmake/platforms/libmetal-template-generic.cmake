ab023c7fc37ccb6760414b463a861695daf8430a
paul <paul@pc-hdy.home>
2023-04-24 16:17:20 +0200

    more options

--- cmake/platforms/libmetal-template-generic.cmake
+++ cmake/platforms/libmetal-template-generic.cmake
@@ -4,7 +4,8 @@
 set (CMAKE_SYSTEM_PROCESSOR "arm"            CACHE STRING "")
 set (MACHINE                "template"       CACHE STRING "")
 set (CROSS_PREFIX           "arm-none-eabi-" CACHE STRING "")
-set (CMAKE_C_FLAGS          "-mcpu=cortex-m4 -std=gnu11 -g3 -DNO_ATOMIC_64_SUPPORT -mfpu=fpv4-sp-d16"               CACHE STRING "")
+set (CMAKE_C_FLAGS          "-mcpu=cortex-m4 -DNO_ATOMIC_64_SUPPORT -mfpu=fpv4-sp-d16 \
+-mfloat-abi=hard -mthumb -static -mlittle-endian -mthumb-interwork -Wl,--gc-sections -Wall -Wextra -pedantic -std=c11 -ffreestanding -flto"               CACHE STRING "")
 
 include (cross-generic-gcc)
 
